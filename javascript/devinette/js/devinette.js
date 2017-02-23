3d024bdf6ffb5590fe54429406dc46f2c824189d
R0ulito <roulit77@gmail.com>
2017-02-23 11:21:23 +0100

    Ajout de l algo en *.md

--- javascript/devinette/js/devinette.js
+++ javascript/devinette/js/devinette.js
@@ -14,29 +14,37 @@ var solution = Math.floor(Math.random() * 100) + 1;
 //console.log("(La solution est " + solution + ")");
 
 // TODO : complétez le programme
-var answer='' , token=0;
+var answer, token=0;
 function answerInput(){
     answer = parseInt(prompt('Devinez le nombre'));
 };
 
-
+answerInput();
 while (token < 6) {
     if (answer < solution) {
         if (answer == '' || isNaN(answer)) {
             answerInput();
         }
         alert('C\'est plus');
-        token += 1;
+        token ++;
+        console.log(token);
         answerInput();
                 
     } else if (answer > solution) {
         alert('C\'est moins');
-        token += 1;
+        token ++;
         answerInput();
-    } else if (token > 6 && answer !== solution){
-        alert('Vous avez perdu. La solution était; ' + solution);
+        console.log(token);
+    } else {
+        break
     }
 }
 
-alert('Vous avez trouvé! La réponse était: ' + solution);
+if (token >= 6 && answer !== solution){
+    alert('Vous avez perdu. La solution était: ' + solution);
+}
+
+if (answer == solution ) {
+    alert('Vous avez trouvé! La réponse était: ' + solution);
+}
 
