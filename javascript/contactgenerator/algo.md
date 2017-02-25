ff1f2045e3f45ba3104d789a11fa5cd011444194
R0ulito <roulit77@gmail.com>
2017-02-25 02:44:01 +0100

    Script fonctionnel

---
+++ javascript/contactgenerator/algo.md
@@ -0,0 +1,25 @@
+var Contacts=[], loop
+var Contact = {
+        init: function (lastName, name) {
+            this.name = name;
+            this.lastName = lastName;
+        },
+        describe: function () {
+            var describtion = "Nom: " + this.lastName + ", prénom: " + this.name;
+            return describtion;
+        }
+    };
+
+Création contact1
+Ajout contact1 au tableau Contacts
+
+Création contact2
+Ajout contact2 au tableau Contacts
+
+
+fonction list()
+    pour chaques contacts afficher nom contact, prénom contact
+
+fonction ajouterContact()
+
+
--- javascript/contactgenerator/js/contacts.js
+++ javascript/contactgenerator/js/contacts.js
@@ -4,7 +4,7 @@ Activité : gestion des contacts
 
 // TODO : complétez le programme
 
-var contacts = [], loop;
+var contacts = [], loop, choices = ["\n0: Quitter le générateur", "1: Lister les contacts", "2: Ajouter un contact\r"];
 var Contact;
 Contact = {
     init: function (lastName, name) {
@@ -16,32 +16,57 @@ Contact = {
         return describtion;
     }
 };
-var contact1 = Object.create(Contact);
-var contact2 = Object.create(Contact);
-contact1.init("Lévisse", "Carole");
-contact2.init('Mélodie', 'Nelsonne');
-
-contacts.push(contact1, contact2);
 
 function contactList() {
+    console.log('\nVoici la liste de tous vos contacts :')
     contacts.forEach(function (Contact) {
         console.log(Contact.describe());
-
     });
 };
 
-contactList();
+function contactAdd () {
+    var lastName = prompt('Entrez le nom du nouveau contact'),
+        name = prompt('Entrez le prénom du nouveau contact')
+    var contact = Object.create(Contact);
+    contact.init(lastName, name);
+    contacts.push(contact)
+    console.log('\nLe contact a bien été ajouté');
+}
 
-var contact3 = Object.create(Contact);
-contact3.init("Monfort", "Julios");
-contacts.push(contact3);
-contactList();
+function choicesList() {
+    for (var i=0; i < choices.length; i ++) {
+        console.log(choices[i]);
+    }
+
+}
+
+var contact1 = Object.create(Contact);
+contact1.init("Lévisse", "Carole");
+
+var contact2 = Object.create(Contact);
+contact2.init('Nelsonne', 'Mélodie');
+
+contacts.push(contact1, contact2);
+
+console.log('Bienvenue dans le gestionnaire de contacts!')
+choicesList();
+
+while (loop = true) {
+    var choice = prompt('Choisissez une option')
+    if (choice == 1) {
+        contactList();
+        choicesList();
+    } else if (choice == 2) {
+        contactAdd();
+        choicesList();
+    } else if (choice == 0) {
+        break
+    }
 
 
-/*while (loop = true) {
 
 
 
 
 
-} */
+}
